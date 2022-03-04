5856df7403bb3fa5ba855c52aa710c97f839fff3
Sudham Jayanthi <workwithsudham@gmail.com>
2022-03-04 20:48:24 +0530

    added contract overview

---
+++ contracts/Comments.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Comments {
+    // Exposed data structure
+    struct Comment {
+        uint32 id;
+        string topic;
+        address creator_address;
+        string message;
+        uint256 created_at;
+    }
+
+    // Notify users that a comment was added
+    event CommentAdded(Comment comment);
+
+    Comment[] public comments;
+
+    // Fetch a list of comments for a topic
+    function getComments(string calldata topic)
+        public
+        view
+        returns (Comment[] memory)
+    {}
+
+    // Persist a new comment
+    function addComment(string calldata topic, string calldata message)
+        public
+    {}
+}

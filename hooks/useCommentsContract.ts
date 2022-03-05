0fca6ae47901fd9b483707ffa3e31a9894d5eaa4
Sudham Jayanthi <workwithsudham@gmail.com>
2022-03-05 23:33:41 +0530

    done with frontend too, ayayay

--- hooks/useCommentsContract.ts
+++ hooks/useCommentsContract.ts
@@ -31,7 +31,7 @@ const useCommentsContract = () => {
 	// We also pass in the signer if there is a signed in wallet, or if there's
 	// no signed in wallet then we'll pass in the connected provider.
 	const contract = wagmi.useContract({
-		addressOrName: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+		addressOrName: "0x3810Ff4a012bEDf71942975D113C7E5B5e0cF87E",
 		contractInterface: CommentsContract.abi,
 		signerOrProvider: signer.data || provider
 	})
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,45 @@
-import type { NextPage } from "next";
-import * as React from "react";
-import { QueryClient, QueryClientProvider, QueryCache } from "react-query";
-import { ChakraProvider, Box, Heading } from "@chakra-ui/react";
-import { Toaster, toast } from "react-hot-toast";
-import theme from "../theme";
-
+import type { NextPage } from "next"
+import * as React from "react"
+import { QueryClient, QueryClientProvider, QueryCache } from "react-query"
+import { ChakraProvider, Box, Heading } from "@chakra-ui/react"
+import { Toaster, toast } from "react-hot-toast"
+import theme from "../theme"
 import { Provider as WagmiProvider } from "wagmi"
 import { providers } from "ethers"
+import Comments from "../components/comments"
 
-const provider = providers.getDefaultProvider()
+// Provider that will be used when no wallet is connected (aka no signer)
+const provider = providers.getDefaultProvider("https://rpc-mumbai.matic.today")
 
 // Create a react-query client
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-  queryCache: new QueryCache({
-    onError: () => {
-      toast.error(
-        "Network Error: Ensure Metamask is connected to the same network that your contract is deployed to."
-      );
-    },
-  }),
-});
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false
+		}
+	},
+	queryCache: new QueryCache({
+		onError: () => {
+			toast.error(
+				"Network Error: Ensure MetaMask is connected to the same network that your contract is deployed to."
+			)
+		}
+	})
+})
 
 const App: NextPage = () => {
-  return (
-    <ChakraProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <Box p={8} maxW="600px" minW="320px" m="0 auto">
-          <Heading>Oops, no comments yet!</Heading>
-          <Toaster position="bottom-right" />
-        </Box>
-      </QueryClientProvider>
-    </ChakraProvider>
-  );
-};
+	return (
+		<WagmiProvider autoConnect provider={provider}>
+			<ChakraProvider theme={theme}>
+				<QueryClientProvider client={queryClient}>
+					<Box p={8} maxW='600px' minW='320px' m='0 auto'>
+						<Comments topic='my-blog-post' />
+						<Toaster position='bottom-right' />
+					</Box>
+				</QueryClientProvider>
+			</ChakraProvider>
+		</WagmiProvider>
+	)
+}
 
-export default App;
+export default App
